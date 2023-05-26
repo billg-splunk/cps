bb6195e337af175c701e1e5f0ffec031db45c278
Bill Grant <billg@splunk.com>
2023-05-26 10:55:23 -0400

    kubernetes mistakes

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 
 export OTEL_SERVICE_NAME="creditprocessorservice"
 export OTEL_PROPAGATORS="tracecontext,baggage,b3multi"
-export OTEL_EXPORTER_OTLP_ENDPOINT="http://$(NODE_IP):4317"
 
 splunk-py-trace python3 main.py
