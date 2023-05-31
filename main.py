07409353aec6c993b75929d305b5f3c508fc136a
Bill Grant <billg@splunk.com>
2023-05-31 11:21:54 -0400

    credit check

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import logging
 import random
 import time
+import requests
 from flask import Flask, request
 from waitress import serve
 
 app = Flask(__name__)
+
+# Last number =
+# 0-5 = Normal
+# 6, 7, 8 = Feature Flags
+# 9 = HipsterCard txns
+# Issues/Processes:
+# - Errors on feature flag 7 or credit scores not between 300-850
+# - Delays on transactions where credit score is 300-580
 
 @app.route('/test')
 def test_it():
@@ -13,24 +22,11 @@ def test_it():
 @app.route('/getScore')
 def get_credit_score():
     customernum = request.args.get('customernum')
-    # Last number =
-    # 0-5 = Normal
-    # 6, 7, 8 = Feature Flags
-    # 9 = HipsterCard txns
-    # Issues/Processes:
-    # - Errors on feature flag 7 or credit scores not between 300-850
-    # - Delays on transactions where credit score is 300-670
 
     score = random.randrange(250, 850, 1)
-    if score < 300:
-        score = -1
     
     if score >= 300 and score < 580:
         addDelays()
-
-    last_digit = int( str( customernum )[-1] )
-
-    #if last_digit == 7 or score == -1:
 
     return str(score)
 
@@ -38,6 +34,17 @@ def addDelays():
     length = random.randrange(2,4)
     time.sleep(length)
 
+@app.route('runCreditCheck')
+def run_credit_check():
+    customernum = request.args.get('customernum')
+    creditscore = request.args.get('score')
+
+    last_digit = int( str( customernum )[-1] )
+
+    if last_digit == 7 or creditscore < 300:
+        requests.get("http://creditcheckextra:777/extra?customernum=" + customernum)
+    
+    return "OK"
 
 if __name__ == '__main__':
     serve(app, port=8899)
